89a4e2b8619e3a3ead11a9d1b47ff3d0cd92836b
MaksymBabenko <Maksym.Babenko.bbn@gmail.com>
2023-06-19 20:09:11 +0200

    first commit

---
+++ src/main/java/sportClub/telegramBot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package sportClub.telegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sportClub/telegramBot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package sportClub.telegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
